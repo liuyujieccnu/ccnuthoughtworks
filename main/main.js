18dc5bd33156e129e6973a0a633eda2042238adc
liuyujieccnu <liuyujie@mails.ccnu.edu.cn>
2017-07-12 12:19:00 +0800

    提交任务五

---
+++ main/main.js
@@ -0,0 +1,35 @@
+module.exports = function main() {
+    let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
+    console.log(`o) 最小值 = ${sequence.minimum()}
+o) 最大值 = ${sequence.maximum()}
+o) 元素数量 = ${sequence.counter()}
+o) 平均值 = ${sequence.average()}`);
+};
+
+class Sequence {
+  constructor(input) {
+    // Write your code here
+    this.input=input;
+  }
+
+  minimum() {
+    // Write your code here
+    this.input.sort(function sortNumber(a,b){
+        return a - b
+      });
+    return this.input[0];
+  }
+
+  maximum() {
+    return this.input[this.input.length-1]
+  }
+
+  counter() {
+    return this.input.length;
+  }
+
+  average() {
+    var sum = eval(this.input.join("+"));
+    return ~~(sum/this.input.length*100)/100;
+  }
+}
